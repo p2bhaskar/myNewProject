ceeebac14ff48f5a818503d8ab5fe21ae8d0ae3b
p2bhaskar <p2bhaskar@gmail.com>
2023-06-17 23:06:16 +0530

    my first commit

---
+++ src/main/java/com/springboot/web/SpringStarter1Application.java
@@ -0,0 +1,13 @@
+package com.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarter1Application.class, args);
+	}
+
+}
